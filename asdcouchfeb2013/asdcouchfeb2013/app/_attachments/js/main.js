afbac2002d980f4d6fbf303b8ee339dec937dfe8
Harry Lee <leelee@fullsail.edu>
2013-02-21 11:12:01 -0500

    on line 23 added code for a listview refresh. added a empty <ul> in the insex.html document. created a #booklist .append created a <'li'> .append created a <a> .attr and a href added var for my data

--- asdcouchfeb2013/asdcouchfeb2013/app/_attachments/js/main.js
+++ asdcouchfeb2013/asdcouchfeb2013/app/_attachments/js/main.js
@@ -1,3 +1,26 @@
 $(document).ready(function() {
-	console.log('I am ready to begin');
+	$.ajax({
+		"url": "_view/books",
+		"dataType": "json",
+		"success": function(data) {
+			$.each(data.rows, function(index, book){
+				var  lists   = book.value.lists;
+				var title    = book.value.title;
+				var author   = book.value.author;
+				var titles   = book.value.titles;
+				var date     = book.value.date;
+				var genre    = book.value.genre;
+				var subject  = book.value.subject;
+				var rate 	 = book.value.rate;
+				var comments = book.value.comments;
+				$('#booklist').append(
+					$('<li>').append(
+						$('<a>').attr("href", "#")
+							.text(title)
+					)
+				);
+			});
+			$('#booklist').listview('refresh');
+		}
+	});
 });
